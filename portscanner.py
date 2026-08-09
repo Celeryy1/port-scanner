9784a01d15b9bb7d6ad49338bae0b28dacecb393
Jessie Garrard <itsuken01@gmail.com>
2026-08-09 15:57:03 -0700

    Add custom port ranges

--- portscanner.py
+++ portscanner.py
@@ -2,33 +2,40 @@ import socket
 import sys
 
 open_ports = []
-target = input("Provide a target: ")
+target = input("Enter target IP or host: ")
+start_port = int(input("Enter starting port: "))
+end_port = int(input("Enter ending port: "))
+    
+print(f"\nScanning {target} from port {start_port} to {end_port}...")
 
-def port_scan(target):
-    for p in range(1, 1023):
-        print(f"Scanning target: {target}")
-        try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.settimeout(0.5)
-                result = s.connect_ex((target, p))
-                if result == 0:
-                    open_ports.append(p)
-                
-        except KeyboardInterrupt:
-            print("\n Scan halted by user")
-            sys.exit()
+def port_scan(target, start_port, end_port):
+    try:
+        target = socket.gethostbyname(target)
+              
+    except socket.gaierror:
+        print("\n Hostname could not be resolved")
+        sys.exit()
+    
+    except KeyboardInterrupt:
+        print("\n Scan halted by user")
+        sys.exit()
+                    
+    except socket.error:
+        print("\ Server not responding")
+        sys.exit()
 
-        except socket.gaierror:
-                print("\n Hostname could not be resolved")
-                sys.exit()
-                
-        except socket.error:
-                print("\ Server not responding")
-                sys.exit()
+    for p in range(start_port, end_port + 1):
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(0.5)
+            result = s.connect_ex((target, p))
+            if result == 0:
+                open_ports.append(p)
 
     print("Open ports:")
-    for p in range(open_ports):
+    for p in open_ports:
         print(p)
 
+    print("Scan complete")
 
-port_scan(target)
+port_scan(target, start_port, end_port)
